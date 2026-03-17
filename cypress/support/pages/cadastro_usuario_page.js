9a06d8c2e04498da9f91b611547de72345747647
Ruth Dantas <ruthdevgp@gmail.com>
2026-03-16 23:42:04 -0300

    feat: create custom command for validating signup error message

--- cypress/support/pages/cadastro_usuario_page.js
+++ cypress/support/pages/cadastro_usuario_page.js
@@ -4,5 +4,11 @@ export default {
   clicarCadastrar() {
     cy.get('#btnRegister')
       .click()
-  }
+  },
+
+  validarMensagemErro(mensagem) {
+    cy.get('.errorLabel')
+      .should('be.visible')
+      .should('have.text', mensagem)
+  },
 }
