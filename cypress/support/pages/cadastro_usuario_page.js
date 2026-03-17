f80265284866e87f08fd2df87a959580f5481cbc
Ruth Dantas <ruthdevgp@gmail.com>
2026-03-16 23:49:53 -0300

    feat: create custom command for validating signup success message

--- cypress/support/pages/cadastro_usuario_page.js
+++ cypress/support/pages/cadastro_usuario_page.js
@@ -11,4 +11,14 @@ export default {
       .should('be.visible')
       .should('have.text', mensagem)
   },
+
+  validarMensagemCadastroSucesso(name){
+    cy.get('.swal2-title')
+          .should('be.visible')
+          .should('have.text', 'Cadastro realizado!')
+    
+    cy.get('.swal2-html-container')
+      .should('be.visible')
+      .should('have.text', `Bem-vindo ${name}`)
+  }
 }
