20a5cb0d0294798309aadf2cf15c51419fa418bf
Ruth Dantas <ruthdevgp@gmail.com>
2026-03-17 14:16:35 -0300

    test: add new login test scenarios

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,6 +2,7 @@
 
 import { user } from '../support/factories/userFactory'
 import commom_page from '../support/pages/commom_page'
+import login_page from '../support/pages/login_page'
 
 describe('Login', () => {
 
@@ -12,21 +13,76 @@ describe('Login', () => {
   })
   it('Login com sucesso', () => {
 
-    cy.get('#user')
-      .type(user.dadosValidos.email)
-    
-    cy.get('#password')
-      .type(user.dadosValidos.senha)
-    
-    cy.get('#btnLogin')
-      .click()
-    
-    cy.get('#swal2-title')
-      .should('be.visible')
-      .should('have.text', 'Login realizado')
-    
-    cy.get('#swal2-html-container')
-      .should('be.visible')
-      .should('have.text', `Olá, ${user.dadosValidos.email}`)
-  })  
+    login_page.preencherEmail(user.dadosValidos.email)
+    login_page.preencherSenha(user.dadosValidos.senha)
+    login_page.clicarLogin()
+    login_page.validarMensagemSucessoLogin(user.dadosValidos.email)
+  })
+  
+  it('Login com todos os campos vazios', () => {
+    login_page.clicarLogin()
+    login_page.validarMensagemErro('E-mail inválido.')
+  })
+
+  it('Login sem informar e-mail', () => {
+    login_page.preencherSenha(user.dadosValidos.senha)
+    login_page.clicarLogin()
+    login_page.validarMensagemErro('E-mail inválido.')
+  })
+
+  it('Login sem informar senha', () => {
+    login_page.preencherEmail(user.dadosValidos.email)
+    login_page.clicarLogin()
+    login_page.validarMensagemErro('Senha inválida.')
+  })
+
+  it('Login informando e-mail inválido', () => {
+    login_page.preencherEmail(user.dadosInvalidos.emailInvalido)
+    login_page.preencherSenha(user.dadosValidos.senha)
+    login_page.clicarLogin()
+    login_page.validarMensagemErro('E-mail inválido.')
+  })
+
+  it('Login informando senha inválida', () => {
+    login_page.preencherEmail(user.dadosValidos.email)
+    login_page.preencherSenha(user.dadosInvalidos.senhaInvalida)
+    login_page.clicarLogin()
+    login_page.validarMensagemErro('Senha inválida.')
+  })
+
+  it('Login com e-mail maiúsculo', () => {
+
+    login_page.preencherEmail(user.dadosValidos.email.toUpperCase())
+    login_page.preencherSenha(user.dadosValidos.senha)
+    login_page.clicarLogin()
+    login_page.validarMensagemSucessoLogin(user.dadosValidos.email.toUpperCase())
+  })
+
+  it('Deve realizar login ao pressionar ENTER', () => {
+    login_page.preencherEmail(user.dadosValidos.email)
+    login_page.preencherSenha(user.dadosValidos.senha)
+
+    cy.get('#password').type('{enter}')
+
+    login_page.validarMensagemSucessoLogin(user.dadosValidos.email)
+  })
+
+  it('Deve redirecionar para cadastro ao clicar em "Ainda não tem conta?"', () => {
+
+    cy.contains('Ainda não tem conta?').click()
+    cy.url().should('include', '/register')
+  })
+
+  it('Deve selecionar o checkbox "Lembrar de mim"', () => {
+    cy.get('#materialUnchecked')
+      .check()
+      .should('be.checked')
+  })
+
+  it('Deve desmarcar o checkbox "Lembrar de mim"', () => {
+    cy.get('#materialUnchecked')
+      .check()
+      .uncheck()
+      .should('not.be.checked')
+  })
 })
