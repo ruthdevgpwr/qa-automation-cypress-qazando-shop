ff6f63c7a32ca78747048200a6c1da7c529928e3
Ruth Dantas <ruthdevgp@gmail.com>
2026-03-17 12:29:48 -0300

    feat: start login test automation

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,32 @@
 /// <reference types="cypress" />
 
+import { user } from '../support/factories/userFactory'
+import commom_page from '../support/pages/commom_page'
+
 describe('Login', () => {
+
+  beforeEach(('Acessar página de Login de Usuário'), () => {
+
+    commom_page.acessarLoginUsuario()
+
+  })
   it('Login com sucesso', () => {
 
-    cy.visit('/')
-
-
+    cy.get('#user')
+      .type(user.dadosValidos.email)
+    
+    cy.get('#password')
+      .type(user.dadosValidos.senha)
+    
+    cy.get('#btnLogin')
+      .click()
+    
+    cy.get('#swal2-title')
+      .should('be.visible')
+      .should('have.text', 'Login realizado')
+    
+    cy.get('#swal2-html-container')
+      .should('be.visible')
+      .should('have.text', `Olá, ${user.dadosValidos.email}`)
   })  
 })
